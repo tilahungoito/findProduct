17ef78991b1d9c98d7d1e539610ea19f480e343b
tilahun goitom <tilahun1goitomg@gmail.com>
2024-11-27 10:30:18 -0800

    almost finalizing

--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,12 @@ import passport from 'passport';
 import connectDB from './config/db.js';
 import productRoutes from './routes/product.routes.js';
 import authRoutes from './routes/auth.routes.js';
-
+import path from 'path';
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+const __dirname = path.resolve();
 // Connect to MongoDB
 connectDB();
 
@@ -24,7 +24,12 @@ app.use(passport.initialize()); // Initialize Passport
 // Routes
 app.use('/api/products', productRoutes);
 app.use('/api/users', authRoutes); // Add authentication routes
-
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'));
+    });
+}
 // Start server
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
